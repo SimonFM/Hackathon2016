d6bea33a900fd4118e606ce16ec5de7939d76eb8
Simon <markhams@tcd.ie>
2016-06-21 22:08:02 +0100

    Added Fetch User

--- server.js
+++ server.js
@@ -26,11 +26,13 @@ app.get('/', function(req, res){
 app.listen(4000);
 console.log('server running ' + 'now ' + Date.now());
 
-var testUser = {name: 'Ajay', age: 42, roles: ['admin', 'moderator', 'user']};
+var testUser = {name: 'Niall', age: 42, roles: ['admin', 'moderator', 'user']};
 
-addUser(testUser);
+//addUser(testUser);
 
-//
+//findUser('Niall');
+
+// Inserts the given user in to the database
 //
 //
 function addUser(user){
@@ -56,4 +58,35 @@ function addUser(user){
     });
   }
 });
+}
+
+// Takes the name of the user we want to find in the database and then returns
+// that user
+//
+function findUser(user){
+  // Use connect method to connect to the Server
+  MongoClient.connect(mongoURL, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      //HURRAY!! We are connected. :)
+      console.log('Connection established to', mongoURL);
+
+      // Get the documents collection
+      var collection = db.collection('users');
+
+      // Insert some users
+      collection.find({name: user}).toArray(function (err, result) {
+        if (err) {
+          console.log(err);
+        } else if (result.length) {
+          console.log('Found:', result);
+        } else {
+          console.log('No document(s) found with defined "find" criteria!');
+        }
+        //Close connection
+        db.close();
+      });
+    }
+  });
 }
