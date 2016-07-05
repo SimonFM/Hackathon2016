0a98bc05f86b6266a7453556fbf040a30499d638
Niall Curran <niallcurran95@gmail.com>
2016-07-05 11:34:27 +0100

    Update merchant.js
    Added phoneNumber String to models

--- models/merchant.js
+++ models/merchant.js
@@ -1,19 +1,16 @@
-/**
- * Created by simonmarkham on 04/07/2016.
- */
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var MerchantSchema   = new Schema({
-    fname: String,
-    lname: String,
+var MerchantSchema = new Schema({
+    name: String,
     email: String,
+    phoneNumber: String,
     password: String,
     address: String,
     creditCardNumber: String,
     expMonth: String,
     expYear: String,
-    cardVeriCode: String,
+    cardVeriCode: String
 });
 
 module.exports = mongoose.model('Merchant', MerchantSchema);
